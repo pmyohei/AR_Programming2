9f2796d4bffebf33b634a8dd48b25ad7845e1ed0
pmyohei <t.yohei@kandg.co.jp>
2022-11-08 17:09:01 +0900

    ・コード整理 ・アニメーション付きmodelの追加

--- app/src/main/java/com/mark/ar_sample/FirstFragment.java
+++ app/src/main/java/com/mark/ar_sample/FirstFragment.java
@@ -21,8 +21,7 @@ import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.animation.ModelAnimation;
-import com.google.ar.sceneform.animation.ModelAnimator;
+import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.Material;
@@ -44,7 +43,7 @@ public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private ArFragment arFragment;
-    private ModelRenderable mRenderable;
+    private ModelRenderable mglbRenderable;
     private ModelRenderable mRedSphereRenderable;
     private ModelRenderable mBlueSphereRenderable;
     private ModelRenderable mRedCubeRenderable;
@@ -65,60 +64,66 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //ArFragmentを保持
+        // ArFragmentを保持
         arFragment = (ArFragment)getChildFragmentManager().findFragmentById(R.id.sceneform_fragment);
 
-        //明示的に完了できる非同期処理クラス
-        //まずは、「AR上に表示させるオブジェクトモデル」のCompletableFutureを生成する
-        CompletableFuture<ModelRenderable> completableFuture =
-            ModelRenderable
-            .builder()
-            //.setSource(view.getContext(), Uri.parse("models/tree.glb"))
-//            .setSource(view.getContext(), Uri.parse("models/halloween.glb"))
-//            .setSource(view.getContext(), Uri.parse("models/sample_bear.glb"))
-            .setSource(view.getContext(), Uri.parse("models/sample_bear_small2.glb"))
-//            .setSource(view.getContext(), Uri.parse("models/test_anim.glb"))
-//            .setSource(view.getContext(), Uri.parse("models/steampunk_vehicle.gltf"))
-            .setIsFilamentGltf(true)    //これは上のファイルを読み込む場合は必要なよう
-            .build();
+        //------------------------------------------
+        // 3Dモデルレンダリング「ModelRenderable」の生成
+        // ------------------------------------------
+        // レンダリングは非同期で生成する
+        CompletableFuture<ModelRenderable> modelCompletableFuture =
+                ModelRenderable
+                        .builder()
+//                      .setSource(view.getContext(), Uri.parse("models/test_anim.glb"))
+//                      .setSource(view.getContext(), Uri.parse("models/tree.glb"))
+//                      .setSource(view.getContext(), Uri.parse("models/halloween.glb"))
+//                        .setSource(view.getContext(), Uri.parse("models/sample_bear_small2.glb"))
+                        .setSource(view.getContext(), Uri.parse("models/sample_bear_ver3_born_anim.glb"))
+//                      .setSource(view.getContext(), Uri.parse("models/steampunk_vehicle.gltf"))
+                        .setIsFilamentGltf(true)    // これは上のファイルを読み込む場合は必要なよう
+                        .build();
 
-        //非同期処理にて、AR上に表示させるオブジェクトモデルを作成
-        completableFuture
-            .thenAccept( renderable -> mRenderable = renderable )
-            .exceptionally(
-                    throwable -> {
-                        Toast toast = Toast.makeText(view.getContext(), "Unable to load andy renderable", Toast.LENGTH_LONG);
-                        toast.setGravity(Gravity.CENTER, 0, 0);
-                        toast.show();
+        // 非同期処理結果として、指定したレンダリングを受け取る
+        modelCompletableFuture
+                .thenAccept( renderable -> mglbRenderable = renderable )
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(view.getContext(), "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
 
-                        Log.i("AR調査", "ModelRenderable　失敗");
-                        return null;
-                    });
+                            Log.i("AR調査", "ModelRenderable　失敗");
+                            return null;
+                        });
 
-        //非同期処理にて、AR上に表示させるオブジェクトモデルを作成
+        //----------------------------------------------
+        // 単純な図形のレンダリング「Material」の生成
+        // ----------------------------------------------
+        // 非同期にてMaterialを生成
         CompletableFuture<Material> materialCompletableFuture =
-            MaterialFactory.makeOpaqueWithColor( view.getContext(), new Color(android.graphics.Color.RED) );
+                MaterialFactory.makeOpaqueWithColor( view.getContext(), new Color(android.graphics.Color.RED) );
 
         //円のモデル生成完了時、生成されたMaterialからモデルを生成して保持する
         materialCompletableFuture
-            .thenAccept(
-                material -> {
-                    //半径／中心／素材
-                    mRedSphereRenderable = ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.15f, 0.0f), material);
+                .thenAccept(
+                        material -> {
+                            //半径／中心／素材
+                            mRedSphereRenderable = ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.15f, 0.0f), material);
 
-                    //！ここで色を変えると、前に作成したRenderableにも影響がでる
-                    //material.setFloat3( MATERIAL_COLOR , new Color(android.graphics.Color.BLUE) );
-                    mBlueSphereRenderable = ShapeFactory.makeSphere(0.05f, new Vector3(0.0f, 0.0f, 0.0f), material);
+                            //！ここで色を変えると、前に作成したRenderableにも影響がでる
+                            //material.setFloat3( MATERIAL_COLOR , new Color(android.graphics.Color.BLUE) );
+                            mBlueSphereRenderable = ShapeFactory.makeSphere(0.05f, new Vector3(0.0f, 0.0f, 0.0f), material);
 
-                    mRedCubeRenderable = ShapeFactory.makeCube(
-                            new Vector3(1.2f, 0.3f, 0.4f),
-                            new Vector3(0.1f, 0.1f, 0.1f),
-                            material);
+                            mRedCubeRenderable = ShapeFactory.makeCube(
+                                    new Vector3(1.2f, 0.3f, 0.4f),
+                                    new Vector3(0.1f, 0.1f, 0.1f),
+                                    material);
+                        });
 
-
-                });
-
-
+        //----------------------------------------------
+        // ビューのレンダリング生成
+        // ----------------------------------------------
         ViewRenderable
                 .builder()
                 .setView(view.getContext(), R.layout.sample_card)
@@ -130,39 +135,38 @@ public class FirstFragment extends Fragment {
             return;
         }
 
-        //平面タップリスナーの設定
+        //----------------------------------------------
+        // 平面タップリスナーの設定
+        // ----------------------------------------------
         arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
-                @Override
-                public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
 
-                    Log.i("AR調査", "onTapPlane");
-                    Log.i("AR調査", "mRenderable=" + mRenderable);
+                // Renderable未生成なら、処理なし
+                if( mglbRenderable == null || mRedSphereRenderable == null || mBlueSphereRenderable == null || mRedCubeRenderable == null ){
+                    return;
+                }
 
-                    //Renderable未生成なら、処理なし
-                    if( mRenderable == null || mRedSphereRenderable == null || mBlueSphereRenderable == null || mRedCubeRenderable == null ){
-                        return;
-                    }
+                // 何を描画するかはランダム
+                Random random = new Random();
+                int num = random.nextInt(3);
 
-                    //何を描画するかはランダム
-                    Random random = new Random();
-                    int num = random.nextInt(3);
+                Renderable renderable;
+                switch (num){
+                    case 0:
+                        renderable = mglbRenderable;
+                        break;
 
-                    Renderable renderable;
-                    switch (num){
-                        case 0:
-                            renderable = mRenderable;
-                            break;
-                        case 1:
-                            renderable = mRedSphereRenderable;
-                            break;
-                        case 2:
-                        default:
-                            renderable = mBlueSphereRenderable;
-                            break;
-                    }
+                    case 1: renderable = mRedSphereRenderable;
+                        break;
+
+                    case 2:
+                    default: renderable = mBlueSphereRenderable;
+                        break;
+                }
 
 //                    renderable = mRedCubeRenderable;
-                    renderable = mRenderable;
+                renderable = mglbRenderable;
 
 //                    Renderable renderable = mRenderable;
                     /*Renderable renderable = mRedSphereRenderable;
@@ -170,75 +174,90 @@ public class FirstFragment extends Fragment {
                         return;
                     }*/
 
-                    Log.i("AR調査", "リスナー働いている2");
+                Log.i("AR調査", "リスナー働いている2");
 
-                    //AnchorNodeの生成
-                    Anchor anchor = hitResult.createAnchor();
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent( arFragment.getArSceneView().getScene() );
+                //----------------------------------
+                // AnchorNodeの生成／Sceneへの追加
+                //----------------------------------
+                // ARScene
+                Scene scene = arFragment.getArSceneView().getScene();
+                // アンカーノードを生成して、Sceneに追加
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent( scene );
 
-                    Vector3 posa = anchorNode.getLocalPosition();
-                    Log.i("メソッド調査", "アンカーノード位置 x=" + posa.x + " y=" + posa.y + " z="  + posa.z);
+                Vector3 posa = anchorNode.getLocalPosition();
+                Log.i("メソッド調査", "アンカーノード位置 x=" + posa.x + " y=" + posa.y + " z="  + posa.z);
 
-                    //ノードに対するジェスチャ
-                    TransformationSystem transformationSystem = arFragment.getTransformationSystem();
+                //----------------------------------
+                // 3DモデルNodeの生成
+                //----------------------------------
+                // AnchorNodeを親として、モデル情報からNodeを生成
+                TransformationSystem transformationSystem = arFragment.getTransformationSystem();
+//                TestAnimationNode modelNode = new TestAnimationNode( transformationSystem );
+                TransformableNode modelNode = new TransformableNode( transformationSystem );
+                modelNode.setParent(anchorNode);
+                modelNode.setRenderable( renderable );
+                modelNode.select();
 
-                    //AnchorNodeを親として、モデル情報からNodeを生成
-                    TestAnimationNode node = new TestAnimationNode( transformationSystem );
-//                    node.setLocalScale( new Vector3( 0.001f, 0.001f, 0.001f ) );
-                    node.setParent(anchorNode);
-                    node.setRenderable( renderable );
-                    node.select();
+                //----------------------------------
+                // 3DモデルNodeへのアニメーション設定
+                //----------------------------------
+                final int durationInMilliseconds = 1000;
+                final float minimumIntensity = 0.0f;
+                final float maximumIntensity = 1.0f;
 
-                    final int durationInMilliseconds = 1000;
-                    final float minimumIntensity = 0.0f;
-                    final float maximumIntensity = 1.0f;
-                    ValueAnimator intensityAnimator = ObjectAnimator.ofFloat( node, "posY", minimumIntensity, maximumIntensity);
-                    intensityAnimator.setDuration(durationInMilliseconds);
-                    intensityAnimator.setRepeatCount(ValueAnimator.INFINITE);
-                    intensityAnimator.setRepeatMode(ValueAnimator.REVERSE);
-//                    intensityAnimator.start();
+                // 上下移動のアニメーション
+//                ValueAnimator intensityAnimator = ObjectAnimator.ofFloat( modelNode, "posY", minimumIntensity, maximumIntensity);
+//                intensityAnimator.setDuration(durationInMilliseconds);
+//                intensityAnimator.setRepeatCount(ValueAnimator.INFINITE);
+//                intensityAnimator.setRepeatMode(ValueAnimator.REVERSE);
+//                intensityAnimator.start();
 
-                    Log.i("アニメーション", "getAnimationCount()=" + node.getRenderableInstance().getAnimationCount() );
+                Log.i("アニメーション", "getAnimationCount()=" + modelNode.getRenderableInstance().getAnimationCount() );
 
-                    //node.getRenderableInstance().animate(true).start();
+                // モデルに付与されたアニメーションの実行
+                modelNode.getRenderableInstance().animate(true).start();
+                Log.i("AR調査", "アニメーション数=" + modelNode.getRenderableInstance().getAnimationCount());
 
-                    node.setOnTapListener(new Node.OnTapListener() {
-                            @Override
-                            public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
-                                Log.i("onTap", "onTapを検出");
-                                //node.setLocalScale( new Vector3( 2, 3, 1 ) );
+                //---------------------------
+                // ノードタッチ検出とビューの生成
+                // ※実験
+                //---------------------------
+                modelNode.setOnTapListener(new Node.OnTapListener() {
+                    @Override
+                    public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
+                        Log.i("onTap", "onTapを検出");
+                        //modelNode.setLocalScale( new Vector3( 2, 3, 1 ) );
 
-                                Vector3 pos = node.getLocalPosition();
-                                Log.i("メソッド調査", "ノード位置(local) x=" + pos.x + " y=" + pos.y + " z="  + pos.z);
-                                Vector3 posw = node.getWorldScale();
-                                Log.i("メソッド調査", "ノード位置(world) x=" + posw.x + " y=" + posw.y + " z="  + posw.z);
-                                Vector3 scale = node.getLocalScale();
-                                Log.i("メソッド調査", "ノードスケール(local) x=" + scale.x + " y=" + scale.y + " z="  + scale.z);
-                                Vector3 scaleW = node.getWorldScale();
-                                Log.i("メソッド調査", "ノードスケール(world) x=" + scaleW.x + " y=" + scaleW.y + " z="  + scaleW.z);
+                        Vector3 pos = modelNode.getLocalPosition();
+                        Vector3 posw = modelNode.getWorldScale();
+                        Vector3 scale = modelNode.getLocalScale();
+                        Vector3 scaleW = modelNode.getWorldScale();
 
-                                Log.i("メソッド調査", "ノードスケール(world) x=" + scaleW.x + " y=" + scaleW.y + " z="  + scaleW.z);
+                        Log.i("メソッド調査", "ノード位置(local) x=" + pos.x + " y=" + pos.y + " z="  + pos.z);
+                        Log.i("メソッド調査", "ノード位置(world) x=" + posw.x + " y=" + posw.y + " z="  + posw.z);
+                        Log.i("メソッド調査", "ノードスケール(local) x=" + scale.x + " y=" + scale.y + " z="  + scale.z);
+                        Log.i("メソッド調査", "ノードスケール(world) x=" + scaleW.x + " y=" + scaleW.y + " z="  + scaleW.z);
+                        Log.i("メソッド調査", "ノードスケール(world) x=" + scaleW.x + " y=" + scaleW.y + " z="  + scaleW.z);
 
-                                if( mTextViewRenderable == null ){
-                                    return;
-                                }
-
-                                //ノードサイズ
-                                Vector3 vector3 = node.getLocalScale();
-
-                                //テキストノードを生成して、タッチされたノードの少し上に表示させる
-                                TransformableNode textNode = new TransformableNode( transformationSystem );
-                                textNode.setParent( node );
-                                textNode.setLocalPosition( new Vector3( -0.5f, 0.3f + 0.0f, 0f ) );
-                                textNode.setRenderable(mTextViewRenderable);
-                                textNode.select();
-                            }
+                        if( mTextViewRenderable == null ){
+                            return;
                         }
-                    );
-                }
+
+                        //ノードサイズ
+                        Vector3 vector3 = modelNode.getLocalScale();
+
+                        //テキストノードを生成して、タッチされたノードの少し上に表示させる
+                        TransformableNode textNode = new TransformableNode( transformationSystem );
+                        textNode.setParent( modelNode );
+                        textNode.setLocalPosition( new Vector3( -0.5f, 0.3f + 0.0f, 0f ) );
+                        textNode.setRenderable(mTextViewRenderable);
+                        textNode.select();
+                    }
+                });
             }
-        );
+        });
     }
 
     @Override
