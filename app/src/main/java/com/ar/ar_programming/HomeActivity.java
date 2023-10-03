6f25f3205ea7665aa03d13dec9b3223b592c3992
pmyohei <t.yohei@kandg.co.jp>
2023-10-03 10:22:58 +0900

    ■広告

--- app/src/main/java/com/ar/ar_programming/HomeActivity.java
+++ app/src/main/java/com/ar/ar_programming/HomeActivity.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Admob
+        initAdmob();
 
         //debug-------------------
         Common.setTutorialDebug(this, 1);
@@ -41,5 +50,22 @@ public class HomeActivity extends AppCompatActivity {
     public void onSettingClicked(View view) {
         Intent intent = new Intent(this, SettingActivity.class);
         startActivity(intent);
+    }
+
+    /*
+     * GoogleMobileAdsSDKの初期化
+     */
+    private void initAdmob(){
+        // AdMob初期化
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        // load
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 }
