3575f49dc0e4da4d9cdbdd4d3d62e614509dfef0
pmyohei <t.yohei@kandg.co.jp>
2023-10-16 13:48:12 +0900

    ■リリース審査不可：ファミリーポリシーの要件対応
    〇注意メッセージの表示
    ARの遊び方のページ先頭に、ARで遊ぶ際の注意事項を記載。記載ポイントは以下の２点。
    ・親の監督の重要性についての適切なメッセージ。
    ・現実世界での物理的な危険に注意するための注意喚起 (周囲に注意するなど)。
    
    〇広告
    ・配信広告にレーティング上限を設定

--- app/src/main/java/com/ar/ar_programming/HomeActivity.java
+++ app/src/main/java/com/ar/ar_programming/HomeActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.admanager.AdManagerAdRequest;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
@@ -59,7 +61,21 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        //----------------------------
+        // 子ども向け広告のリクエスト設定
+        //----------------------------
+        RequestConfiguration requestConfiguration =
+                MobileAds.getRequestConfiguration()
+                    .toBuilder()
+                    .setTagForChildDirectedTreatment(
+                        RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+                    .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
+                    .build();
+        MobileAds.setRequestConfiguration(requestConfiguration);
+        
+        //----------------------------
         // load
+        //----------------------------
         AdView adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
